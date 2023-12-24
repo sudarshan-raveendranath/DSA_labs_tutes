d88bd83dba7055dd74e8a61571d595daaa78183f
Sudarshan Raveendranath <120754357+sudarshan-raveendranath@users.noreply.github.com>
2023-12-24 18:52:09 +0530

    Create radiusCalculator.kt

---
+++ MAD/Tutes/Tute1/radiusCalculator.kt
@@ -0,0 +1,12 @@
+fun radiusCalculator(r : Double): Double {
+    var permeter = 3.14.times(r) * 2.0
+    return 3.14 * r * 2
+}
+
+fun main() {
+    var modules = arrayOf("MAD","DSA","ITP","ESD","PS")
+    println(modules[0])
+
+    println(radiusCalculator(5.0))
+}
+
